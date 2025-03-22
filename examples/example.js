17e3f932590cd82f2c58e2ea1e1878d91a08b68d
Ur Koren <ork14790@gmail.com>
2025-03-22 23:32:31 +0200

    Update Example

--- examples/example.js
+++ examples/example.js
@@ -3,14 +3,14 @@ const Terraform = require("../src/terraform");
 const tf = new Terraform();
 
 // 1. Create an S3 bucket (example)
-tf.create("aws_s3_bucket", { bucket: "my-example-bucket" });
+tf.create("aws_s3_bucket", { bucket: Date.now() });
 
 // 2. Initialize Terraform
-tf.init();
+tf.init({ provider: "aws", region: "il-central-1" });
 
 // 3. Apply the Terraform configuration
 tf.apply();
 
 // 4. Destroy resources (if needed)
-// tf.destroy();
+tf.destroy();
 
